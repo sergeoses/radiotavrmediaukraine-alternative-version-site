e37cc790c621945a27ebfb19711b43d120953df7
Serhii Sarnavskyi <94855406+sergeoses@users.noreply.github.com>
2023-08-03 20:05:46 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,34 +1,4 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Ваш блог</title>
-     <link rel="stylesheet" href="style.css">
-    <!-- Підключіть ваші стилі CSS -->
-</head>
-<body>
-    <!-- Ваша шапка сайту -->
-    <header>
-         <nav>
-        <ul>
-            <li><a href="HitFm.html">Перехід на радіостанцію Хіт ФМ</a></li>
-            <li><a href="RadioRocks.html">Перехід на радіостанцію РадіоРокс</a>
-         </ul>
-        </nav>
-        <!-- Меню та інші елементи шапки -->
-    </header>
-
-    <!-- Блок для написання дописів -->
-    <section>
-        <h2>Напишіть допис</h2>
-        <form method="post" action="save_post.php">
-            <input type="text" name="title" placeholder="Заголовок допису" required>
-            <textarea name="content" placeholder="Текст допису" required></textarea>
-            <button type="submit">Зберегти допис</button>
-        </form>
-    </section>
-
-    <!-- Виведення дописів -->
-    <section>
+<section>
         <h2>Дописи</h2>
         <ul>
             <?php
@@ -41,10 +11,3 @@
             ?>
         </ul>
     </section>
-
-    <!-- Ваш підвал сайту -->
-    <footer>
-        <!-- Елементи підвалу -->
-    </footer>
-</body>
-</html>
