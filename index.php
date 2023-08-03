8982c8bed88d470553dc7d405b639a7f415d26f4
Serhii Sarnavskyi <94855406+sergeoses@users.noreply.github.com>
2023-08-03 19:45:36 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,44 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Блог</title>
+    <!-- Підключіть ваші стилі CSS -->
+</head>
+<body>
+    <!-- Ваша шапка сайту -->
+    <header>
+        <!-- Меню та інші елементи шапки -->
+    </header>
+
+    <!-- Блок для написання дописів -->
+    <section>
+        <h2>Напишіть допис</h2>
+        <form method="post" action="save_post.php">
+            <input type="text" name="title" placeholder="Заголовок допису" required>
+            <textarea name="content" placeholder="Текст допису" required></textarea>
+            <button type="submit">Зберегти допис</button>
+        </form>
+    </section>
+
+    <!-- Виведення дописів -->
+    <section>
+        <h2>Дописи</h2>
+        <ul>
+            <?php
+            // Підключіть ваш код PHP для виведення дописів з бази даних або файлу
+            // Наприклад, використовуючи цикл для перебору дописів
+            // $posts - масив з дописами (заголовок, текст і т.д.)
+            foreach ($posts as $post) {
+                echo "<li><h3>" . $post['title'] . "</h3><p>" . $post['content'] . "</p></li>";
+            }
+            ?>
+        </ul>
+    </section>
+
+    <!-- Ваш підвал сайту -->
+    <footer>
+        <!-- Елементи підвалу -->
+    </footer>
+</body>
+</html>
 
